e1cb5cdc58afb3911f8e8beed98818128fefa60a
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-18 14:23:24 +0200

    +

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 import datetime
+from datetime import date
 from flask import Flask
 
 app = Flask(__name__)
 
-lombard = []
-#number = 0
-#item_name = ''
-
-
 @app.route('/')
-from datetime import date
 today = datetime.datetime.now()
 bday = datetime.datetime(2023,12,16,14,45)
 time_diff = bday - today
