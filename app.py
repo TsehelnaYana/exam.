5f419ee7aba142fc54a909aa5640690d41f4e88f
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-18 14:37:58 +0200

    +

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 import datetime
 from datetime import date
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
 today = datetime.datetime.now()
 bday = datetime.datetime(2023,12,16,14,45)
 time_diff = bday - today
 print(f"Your birthday is in {time_diff}")
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
