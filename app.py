5661707c149a96465bb2d79fab6e69d99ab2cc02
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-18 15:54:27 +0200

    +

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ today = datetime.datetime.now()
 bday = datetime.datetime(2023,12,16,14,45)
 time_diff = bday - today 
 print'(f"Your birthday is in {time_diff}")'
+@app.route('/')
+def hello_world():
+    print(f"Exam")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
