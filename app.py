ec2b7ee674f34c321f0f635410482aaaf2a62382
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-18 15:01:21 +0200

    +

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 today = datetime.datetime.now()
 bday = datetime.datetime(2023,12,16,14,45)
 time_diff = bday - today
-print(f"Your birthday is in {time_diff}")
 
 @app.route('/')
 print(f"Your birthday is in {time_diff}")
