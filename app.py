a98b02b75ab28efe8c6bd4b33b83bfc9d3290959
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-18 21:18:29 +0200

    +

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ def hello_world():
     today = datetime.datetime.now()
     bday = datetime.datetime(2023,12,16,14,45)
     time_diff = bday - today 
-    time = datetime.strftime(time_diff)
+    time = time_diff.strftime("%j")
     return('Tsehelna Yana.Your birthday is in' + time)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
+
 
