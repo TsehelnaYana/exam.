0d1cc21fbd6df2a9a1b21b5d938f35f5b9c1f710
TsehelnaYana <117515088+TsehelnaYana@users.noreply.github.com>
2022-12-19 14:47:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,16 @@
-import unittest
-import app as tested_app
-import json
-
 class FlaskAppTests(unittest.TestCase):
 
 	def setUp(self):
-		self.app = tested_app.test_client()
+	    self.app = app.app.test_client()
 
-	def test_get_hello_endpoint(self):
-		r = self.app.get('/')
-		self.assertEqual(r.status, '200 OK')
-		self.assertEqual(r.data, 157)
-		self.assertIsNotNone(r.data)
-		self.assertGreater(r.data, 0)
+        def test_app(self):
+            r = self.app.get('/')
+	    self.assertEqual(r.status, '200 OK')
+
+	def test_sumOfAP(self):
+	    self.assertEqual(r.data, 157)
+	    self.assertIsNotNone(r.data)
+	    self.assertGreater(r.data, 0)
 
 if __name__ =='__main__':
-	import xmlrunner
-	runner = xmlrunner.XMLTestRunner(output='test-reporst')
-	unittest.main(testRunner=runner)
 	unittest.main()
